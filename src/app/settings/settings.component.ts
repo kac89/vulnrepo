4ff32d941d927a21f2885aa252639bc50e770a5b
Kacper <admin@devilteam.pl>
2019-06-19 20:38:19 +0200

    setting update

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -11,6 +11,9 @@ export class SettingsComponent implements OnInit {
   info = '';
   checked = false;
   hide = false;
+  wipechecked = false;
+  wipehide = false;
+  wipemsg = '';
   advhtml: any;
   advlogo: any;
   settings: any;
@@ -60,6 +63,27 @@ export class SettingsComponent implements OnInit {
 
   }
 
+  wipeDatachanged() {
+
+    if (this.wipechecked === true) {
+      this.wipehide = true;
+    }
+    if (this.wipechecked === false) {
+      this.wipehide = false;
+    }
+
+  }
+
+
+  wipealldata() {
+
+    const remove_settings = indexedDB.deleteDatabase('vulnrepo-settings');
+    const remove_myreports = indexedDB.deleteDatabase('vulnrepo-db');
+
+    this.wipemsg = 'Deleted database successfully!';
+
+  }
+
   saveadvHTML(logo, htmlcontent) {
 
     const keys: string[] = new Array('advenabled', 'advlogo', 'advhtml');
@@ -77,5 +101,101 @@ export class SettingsComponent implements OnInit {
   }
 
 
+
+
+  dumpallmyreports() {
+
+    this.indexeddbService.getReports().then(data => {
+      if (data) {
+        // download dump
+        const element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + JSON.stringify(data));
+        element.setAttribute('download', 'Dumped My Reports (vulnrepo.com).txt');
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+
+      } else {
+        console.log('DB read error');
+      }
+
+    });
+
+  }
+
+
+
+  restoreMyReports(input: HTMLInputElement) {
+
+    const files = input.files;
+
+    if (files && files.length) {
+      /*
+       console.log("Filename: " + files[0].name);
+       console.log("Type: " + files[0].type);
+       console.log("Size: " + files[0].size + " bytes");
+       */
+
+
+      const fileToRead = files[0];
+      const fileReader = new FileReader();
+
+
+      fileReader.onload = (e) => {
+        console.log(fileReader.result);
+      };
+
+      fileReader.readAsText(fileToRead, 'UTF-8');
+    }
+
+  }
+
+
+  exportconfiguration() {
+
+    this.indexeddbService.getSettings().then(data => {
+      if (data) {
+        // download dump
+        const element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + JSON.stringify(data));
+        element.setAttribute('download', 'Settings configuration dump (vulnrepo.com).txt');
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+
+      } else {
+        console.log('Settings read error');
+      }
+
+    });
+
+  }
+
+  restoreConfiguration(input: HTMLInputElement) {
+
+    const files = input.files;
+
+    if (files && files.length) {
+      /*
+       console.log("Filename: " + files[0].name);
+       console.log("Type: " + files[0].type);
+       console.log("Size: " + files[0].size + " bytes");
+       */
+
+
+      const fileToRead = files[0];
+      const fileReader = new FileReader();
+
+
+      fileReader.onload = (e) => {
+        console.log(fileReader.result);
+      };
+
+      fileReader.readAsText(fileToRead, 'UTF-8');
+    }
+
+  }
 
 }
