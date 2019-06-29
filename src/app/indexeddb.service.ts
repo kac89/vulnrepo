31702393ec78d0e420daa018334abff39bd73a18
Kacper <admin@devilteam.pl>
2019-06-29 14:47:09 +0200

    email fix

--- src/app/indexeddb.service.ts
+++ src/app/indexeddb.service.ts
@@ -135,7 +135,8 @@ export class IndexeddbService {
           researcher: {
             reportername: '',
             reportersocial: '',
-            reporterwww: ''
+            reporterwww: '',
+            reporteremail: ''
           }
         };
 
