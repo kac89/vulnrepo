790c5938c014ed915123c0fdd6aed2d59e9737df
Kacper <admin@devilteam.pl>
2020-02-18 21:09:37 +0100

    change key fix hide variable

--- src/app/dialog-changekey/dialog-changekey.component.ts
+++ src/app/dialog-changekey/dialog-changekey.component.ts
@@ -8,6 +8,7 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 })
 export class DialogChangekeyComponent implements OnInit {
   alert: string;
+  hide = true;
 
   constructor(public dialogRef: MatDialogRef<DialogChangekeyComponent>) { }
 
