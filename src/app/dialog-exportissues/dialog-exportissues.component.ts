bf6a7e5ba73ceb8d8622c13e470b6ce530572454
Kacper <admin@devilteam.pl>
2020-03-25 09:51:14 +0100

    fix issues export from .nessus file

--- src/app/dialog-exportissues/dialog-exportissues.component.ts
+++ src/app/dialog-exportissues/dialog-exportissues.component.ts
@@ -43,9 +43,6 @@ export class DialogExportissuesComponent implements OnInit {
 
   jiraCloudExport(jira_c_url, jira_c_key, jira_c_email, jira_c_label, workflow) {
 
-
-
-
     function sevret(text) {
       let ret = 0;
 
@@ -80,8 +77,8 @@ export class DialogExportissuesComponent implements OnInit {
         .replace('$key', jira_c_key)
         .replace('$title', item.title)
         .replace('$desc', item.desc)
-        .replace('$poc', item.poc.replace(/\n/g, '\\n'))
-        .replace('$ref', item.ref.replace(/\n/g, '\\n'))
+        .replace('$poc', item.poc.toString().replace(/\n/g, '\\n'))
+        .replace('$ref', item.ref.toString().replace(/\n/g, '\\n'))
         .replace('$severity', sevret(item.severity))
         .replace('$label', jira_c_label);
 
