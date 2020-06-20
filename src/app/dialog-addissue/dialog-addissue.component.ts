8bd6ad0ec20a009d23254cd70670df161b8f54fe
Kacper <admin@devilteam.pl>
2020-06-21 00:57:24 +0200

    add cve online database integration

--- src/app/dialog-addissue/dialog-addissue.component.ts
+++ src/app/dialog-addissue/dialog-addissue.component.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 export interface Vulns {
   title: string;
@@ -24,13 +25,16 @@ export interface Vulns {
 })
 export class DialogAddissueComponent implements OnInit {
   myControl = new FormControl();
+  mycve = new FormControl();
   options: Vulns[] = [];
   filteredOptions: Observable<Vulns[]>;
   err_msg: string;
-
+  sourceSelect = 'VULNREPO';
+  show = false;
 
   constructor(public router: Router,
-    public dialogRef: MatDialogRef<DialogAddissueComponent>, private http: Http, private datePipe: DatePipe) {
+    public dialogRef: MatDialogRef<DialogAddissueComponent>, private http: Http,
+    private apiService: ApiService, private datePipe: DatePipe) {
 
     this.filteredOptions = this.myControl.valueChanges
       .pipe(
@@ -63,18 +67,15 @@ export class DialogAddissueComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  addIssue(data) {
-
-    if (data !== '') {
+  addIssue() {
+    const data = this.myControl.value;
+    if (data !== '' && data !== null) {
       for (const key in this.options) {
         if (this.options.hasOwnProperty(key)) {
 
           if (this.options[key].title === data) {
-            console.log('found');
-
             const date = new Date();
             const today = this.datePipe.transform(date, 'yyyy-MM-dd');
-
             const def = {
               title: this.options[key].title,
               poc: this.options[key].poc,
@@ -112,7 +113,6 @@ export class DialogAddissueComponent implements OnInit {
         }
       }
     } else {
-      console.log('Empty title!!!');
       this.err_msg = 'Please add title!';
     }
 
@@ -122,6 +122,142 @@ export class DialogAddissueComponent implements OnInit {
     return template ? template.title : undefined;
   }
 
+  changeselect() {
+    this.err_msg = '';
+    this.show = false;
+  }
 
+  addCVE() {
+    this.err_msg = '';
+    let severity = 'Info';
+    const severityRatings = [{
+      name: 'Info',
+      bottom: 0.0,
+      top: 0.0
+    }, {
+      name: 'Low',
+      bottom: 0.1,
+      top: 3.9
+    }, {
+      name: 'Medium',
+      bottom: 4.0,
+      top: 6.9
+    }, {
+      name: 'High',
+      bottom: 7.0,
+      top: 8.9
+    }, {
+      name: 'Critical',
+      bottom: 9.0,
+      top: 10.0
+    }];
+
+    const data = this.mycve.value;
+    if (data !== '' && data !== null) {
+
+      const reg = new RegExp(/^CVE-\d{4}-\d{4,7}$/, 'i');
+      if (reg.test(data)) {
+
+        this.show = true;
+        this.apiService.getCVE(data).then(resp => {
+
+          if (resp !== null && resp !== undefined) {
+            // if everything OK
+            if (resp.id) {
+              const date = new Date();
+              const today = this.datePipe.transform(date, 'yyyy-MM-dd');
+
+              let cvetitle = '';
+
+              if (resp.refmap) {
+
+                if (resp.refmap.xf) {
+                  cvetitle = resp.refmap.xf;
+                }
+                if (resp.refmap.vupen) {
+                  cvetitle = resp.refmap.vupen;
+                }
+                if (resp.refmap.mlist) {
+                  cvetitle = resp.refmap.mlist[0];
+                }
+                if (resp.refmap.bugtraq) {
+                  cvetitle = resp.refmap.bugtraq[0];
+                }
+                if (resp.refmap.idefense) {
+                  cvetitle = resp.refmap.idefense;
+                }
+
+              }
+
+              if (resp.saint) {
+                cvetitle = resp.saint[0].description;
+              }
+
+              if (resp.redhat) {
+                if (resp.redhat.advisories) {
+                  if (resp.redhat.advisories[0].bugzilla) {
+                    cvetitle = resp.redhat.advisories[0].bugzilla.title;
+                  }
+                }
+              }
+
+              if (cvetitle === '') {
+                cvetitle = resp.id;
+              }
+
+              if (resp.cvss) {
+                for (let _i = 0; _i < severityRatings.length; _i++) {
+                  if (severityRatings[_i].bottom <= resp.cvss && severityRatings[_i].top >= resp.cvss) {
+                    severity = severityRatings[_i].name;
+                  }
+                }
+              }
+              let refer = '';
+              if (resp.references) {
+
+                for (let _i = 0; _i < resp.references.length; _i++) {
+                  refer += resp.references[_i] + '\n';
+                }
+
+              }
+
+              const def = {
+                title: cvetitle,
+                poc: '',
+                files: [],
+                desc: resp.summary,
+                severity: severity,
+                ref: refer,
+                cvss: resp.cvss,
+                cve: resp.id,
+                date: today + ''
+              };
+              this.show = false;
+              this.dialogRef.close(def);
+            }
+
+            if (resp.error) {
+              this.err_msg = resp.error;
+              this.show = false;
+            }
+
+          } else {
+            this.show = false;
+            this.err_msg = 'CVE not found.';
+          }
+
+        });
+
+      } else {
+        this.show = false;
+        this.err_msg = 'CVE format error.';
+      }
+
+    } else {
+      this.show = false;
+      this.err_msg = 'Empty field?.';
+    }
+
+  }
 
 }
