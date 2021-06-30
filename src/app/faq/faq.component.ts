2acdea9d2d3bed524f577fad58bf01aef4690d44
Kacper <admin@devilteam.pl>
2021-06-30 10:39:58 +0200

    fix

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute, Params} from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-faq',
