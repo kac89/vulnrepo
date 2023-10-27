e757e5f191d634b4ca41507536a98e0e8a6c8568
Kacper <admin@devilteam.pl>
2023-10-27 21:47:23 +0200

    create pcidssv4 auditor

---
+++ src/app/pcidss4/pcidss4.component.ts
@@ -0,0 +1,182 @@
+import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
+import { HttpClient } from '@angular/common/http';
+
+export interface pcidssElement {
+  id: string;
+  description: string;
+  milestone: number;
+  saq: string[];
+}
+
+@Component({
+  selector: 'app-pcidss4',
+  templateUrl: './pcidss4.component.html',
+  styleUrls: ['./pcidss4.component.scss']
+})
+export class Pcidss4Component implements OnInit {
+  selectlevel: string;
+  selectsaq: string;
+  displayedColumns: string[] = ['select', 'id', 'description', 'milestone', 'saq'];
+  dataSource = new MatTableDataSource<pcidssElement>();
+  selection = new SelectionModel<pcidssElement>(true, []);
+  pcidssdata = [];
+  pcidss:any;
+  selectsaqmodel = [
+    { value: 'All', viewValue: '--- Show all ---' },
+    { value: 'SAQ A', viewValue: 'SAQ A - Card-not-present merchants (e-commerce or mail/telephone order)' },
+    { value: 'SAQ A-EP', viewValue: 'SAQ A-EP - e-Commerce merchants' },
+    { value: 'SAQ B', viewValue: 'SAQ B - Brick and mortar or mail/telephone order merchants' },
+    { value: 'SAQ B-IP', viewValue: 'SAQ B-IP - Brick and mortar or mail/telephone order merchants' },
+    { value: 'SAQ C', viewValue: 'SAQ C - Brick and mortar or mail/telephone order merchants' },
+    { value: 'SAQ C-VT', viewValue: 'SAQ C-VT - Brick and mortar or mail/telephone order merchants' },
+    { value: 'SAQ P2PE', viewValue: 'SAQ P2PE - Brick and mortar or mail/telephone order merchants' },
+    { value: 'SAQ D (Merchants)', viewValue: 'SAQ D - Merchants' },
+    { value: 'SAQ D (Service providers)', viewValue: 'SAQ D - Service providers' }
+  ];
+
+  constructor(private http: HttpClient){}
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: pcidssElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
+  }
+
+  resetselected(){
+    if (window.confirm("Do you really want to clear results?")) {
+      this.selection.clear();
+    }
+  }
+
+  parsefunct(arr){
+
+    let out:any;
+    out = arr
+    if (this.selectlevel !== 'All') {
+      out = arr.filter((item) => {
+        if (item.milestone === Number(this.selectlevel)) {
+          return item;
+        }
+      });
+    }
+
+    if (this.selectsaq !== 'All') {
+      out = arr.filter((item) => {
+        if (item.saq.some(x => x === this.selectsaq)) {
+          return item;
+        }
+      });
+    }
+
+    if (this.selectsaq !== 'All' && this.selectlevel !== 'All') {
+      out = arr.filter((item) => {
+        if (item.milestone === Number(this.selectlevel) && item.saq.some(x => x === this.selectsaq)) {
+          return item;
+        }
+      });
+    }
+    
+
+    return out
+  }
+  parseSAQ(item){
+    return item.join("<br>");
+  }
+
+  isAllSelectedGv(name) {
+
+    name = name + '.';
+    const regex = new RegExp('^'+name+'*');
+    const filterbyname = this.dataSource.data.filter((item) => {
+      if (regex.test(item.id)) {
+        return item;
+      }
+    });
+  
+    const numSelected = this.selection.selected.length;
+    const numRows = filterbyname.length;
+    return numSelected === numRows;
+  }
+  
+  isAllSelectedGroup(items) {
+  
+    const numSelected = this.selection.selected.length;
+    const numRows = items.length;
+    return numSelected === numRows;
+  }
+  
+  checkedfn(name){
+  
+    name = name + '.';
+    const regex = new RegExp('^'+name+'*');
+
+    const filterbynameselected = this.selection.selected.filter((item) => {
+      if (regex.test(item.id)) {
+        return item;
+      }
+    });
+  
+    if (filterbynameselected.length > 0) {
+      return true;
+    } else {
+      return false;
+    }  
+  }
+
+
+  toggleSpecificGroup(name) {
+
+    name = name + '.';
+    const regex = new RegExp('^'+name+'*');
+    
+    const filterbyname = this.dataSource.data.filter((item) => {
+      
+      if (regex.test(item.id)) {
+        return item;
+      }
+    });
+  
+    const filterbynameselected = this.selection.selected.filter((item) => {
+      if (regex.test(item.id)) {
+        return item;
+      }
+    });
+  
+    if (filterbynameselected.length === filterbyname.length) {
+      this.selection.deselect(...filterbyname);
+      return;
+    }
+  
+    this.selection.select(...filterbyname);
+  }
+
+  ngOnInit() {
+    this.selectlevel = 'All';
+    this.selectsaq = 'All';
+
+    this.http.get<any>('/assets/pcidssv4.json?v=' + + new Date()).subscribe(res => {
+      this.pcidss = res;
+
+      for (let item of this.pcidss.items) {
+        for (let subitem of item.subitems) {
+          this.pcidssdata = this.pcidssdata.concat(subitem.litems); 
+        }   
+      }   
+
+      this.dataSource.data = this.pcidssdata;
+    });
+
+
+
+  }
+}
