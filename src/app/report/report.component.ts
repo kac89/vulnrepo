f6df9cbed3ffc13f714b5c58e53676affe86732d
kac89 <kacper@kacper.cc>
2026-04-04 22:53:30 +0200

    dialog remove up

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -872,6 +872,7 @@ export class ReportComponent implements OnInit, OnDestroy, AfterViewInit {
 
     const dialogRef = this.dialog.open(DialogRemoveitemsComponent, {
       width: '400px',
+      panelClass: 'remove-dialog-panel',
       data: { sel: array, orig: this.decryptedReportDataChanged.report_vulns }
     });
 
